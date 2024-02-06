6465bdd964e75fea70a8af20a2d5a5b5268c6b73
azizHr <azizharkati20@gmail.com>
2024-02-06 17:57:36 +0100

    first-commit

---
+++ src/main/java/com/example/madrassaty/MadrassatyApplication.java
@@ -0,0 +1,13 @@
+package com.example.madrassaty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadrassatyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadrassatyApplication.class, args);
+    }
+
+}
